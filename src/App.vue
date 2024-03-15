5bcd9404f127234e13ef429c8cd851526826aa14
rafaelabarbieric <barbieridacruzrafaela@gmail.com>
2024-03-15 15:10:35 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+  import { computed, ref } from 'vue'
+
+  const texto = ref('')
+
+</script>
+
+<template>
+
+  <h1>Formulário 1</h1>
+  <input type="text" v-model="texto" placeholder="Seu nome">
+  <p>{{ texto }}</p>
+  
+</template>
+
+<style scoped>
+
+</style>
